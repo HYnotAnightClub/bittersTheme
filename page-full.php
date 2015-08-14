3d67ec513b11dfff7d0faeaead1bbe048c6312ab
Amanda Milburn <milburn.amanda@gmail.com>
2015-08-14 12:00:46 -0400

    Style updates

--- page-full.php
+++ page-full.php
@@ -9,37 +9,37 @@ get_header();  ?>
 <div class="custom-hero-image" style="background-image: url('<?php header_image(); ?>')">
       <!-- <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /> -->
 </div>
+<div class="blank"></div>
     <!-- THIS IS THE CUSTOM IMAGE GOING AWAY -->
 <div class="main">
-  <div class="container" >
-
-    <?php // Start the loop ?>
-    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-    	<?php  
-    	$thumb_id = get_post_thumbnail_id();
-    	$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
-    	$thumb_url = $thumb_url_array[0];
-    	?>
-		
-		<?php if ( has_post_thumbnail( $post_id ) ){ ?>
-			<div class="mainPost" style="background-image: url('<?php echo $thumb_url; ?>')">
-		      <h2><?php the_title(); ?></h2> 
-		      <?php the_content(); ?>
-			</div>	
-		<?php } else {?>
-			<div class="mainPost" style="background-image: url('<?php bloginfo('template_url'); ?>/images/dark_wall.png')"> <!-- change default background image -->
-			      <h2><?php the_title(); ?></h2> <!-- add your styling here -->
-			      <?php the_content(); ?>
-			</div>	
-		<?php } ?>
-		
-	
-
-		    
-		
-    <?php endwhile; // end the loop?>
-  </div> <!-- /.container -->
-  <?php  dynamic_sidebar( 'body-widget-area' ); ?> <!-- instagram goes here -->
+	<div class="container" >
+	    <?php // Start the loop ?>
+	    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	    	<?php  
+	    	$thumb_id = get_post_thumbnail_id();
+	    	$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
+	    	$thumb_url = $thumb_url_array[0];
+	    	?>
+			
+			<?php if ( has_post_thumbnail( $post_id ) ){ ?>
+				<div class="mainPost" style="background-image: url('<?php echo $thumb_url; ?>')">
+			    	<div class="postText">
+			      		<h2><?php the_title(); ?></h2> 
+			      		<?php the_content(); ?>
+			      	</div>
+				</div>	
+			<?php } else {?>
+				<div class="mainPost" style="background-image: url('<?php bloginfo('template_url'); ?>/images/dark_wall.png')"> <!-- change default background image -->
+					<div class="postText">
+						<h2><?php the_title(); ?></h2> <!-- add your styling here -->
+					    <?php the_content(); ?>
+				    </div>
+				</div>	
+			<?php } ?>		
+	    <?php endwhile; // end the loop?>
+    </div> <!-- /.container -->
+    <div class="blank"></div>
+	<?php  dynamic_sidebar( 'body-widget-area' ); ?> <!-- instagram goes here -->
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
